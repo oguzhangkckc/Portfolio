e4c27532cfa2b7ad16468f43b473231ae39bae53
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-03-28 15:41:06 +0300

    eklemeler

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -19,37 +19,46 @@ export default function NavBar() {
     }
   }
 
-  // Add event listener to handle scrolling
   React.useEffect(() => {
     window.addEventListener('scroll', handleScroll)
 
-    // Remove event listener on cleanup
     return () => window.removeEventListener('scroll', handleScroll)
   }, [])
 
   return (
     <section
-      className={`w-screen fixed top-0 z-50 ${
-        isScrolled ? "bg-white shadow-lg transition duration-300" : ""
-      }`}
+      className={`w-screen fixed top-0 z-50 ${isScrolled ? "bg-white shadow-lg transition duration-300" : ""
+        }`}
     >
       <nav className='items-row w-screen p-3 flex'>
         <div className='flex gap-4 pl-4 flex-none items-end'>
           <Image src={ogi} width={35} height={35} className='rounded-full' />
-          <Link to='me' spy={true} smooth={true} duration={500} className='text-2xl font-extrabold dark:text-slate-300'>Oğuzhan Gökcekoca</Link>
+          <Link to='me' spy={true} smooth={true} duration={500} className='text-2xl font-extrabold dark:text-slate-300 hover:text-teal-500 transition-colors duration-500'>Oğuzhan Gökcekoca</Link>
         </div>
         <div className='flex gap-4 flex-grow justify-end'>
           <ul className='flex gap-28 pr-24 mt-2 font-extrabold dark:text-slate-300'>
-            <li className={`text-lg ${isScrolled ? isActiveLink : isNotActiveLink}`}><Link to='about' spy={true} smooth={true} duration={500}>About</Link></li>
-            <li className={`text-lg ${isScrolled ? isActiveLink : isNotActiveLink}`}><Link to='more' spy={true} smooth={true} duration={500}>More</Link></li>
-            <li className={`text-lg ${isScrolled ? isActiveLink : isNotActiveLink}`}><Link to='contact' spy={true} smooth={true} duration={500}>Contact</Link></li>
+            <li className={`text-lg`}>
+              <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-teal-500 transition-colors duration-500'>
+                About
+              </Link>
+            </li>
+            <li className={`text-lg`}>
+              <Link to='more' spy={true} smooth={true} duration={500} className='hover:text-teal-500 transition-colors duration-500'>
+                More
+              </Link>
+            </li>
+            <li className={`text-lg`}>
+              <Link to='contact' spy={true} smooth={true} duration={500} className='hover:text-teal-500 transition-colors duration-500'>
+                Contact
+              </Link>
+            </li>
           </ul>
         </div>
-        <div className='text-3xl flex gap-8 mt-2 flex-none dark:text-slate-400'>
-          <a href='https://www.linkedin.com/in/oğuzhan-gökcekoca-442802250/'><AiFillLinkedin /></a>
-          <a href='https://github.com/oguzhangkckc'><AiFillGithub /></a>
-          <a href='mailto:ogokcekoca@gmail.com'><AiFillMail /></a>
-          <a href='https://www.instagram.com/oguzhangokcekoca/'><AiFillInstagram /></a>
+        <div className='text-3xl flex gap-8 mt-2 flex-none dark:text-slate-400 '>
+          <a href='https://www.linkedin.com/in/oğuzhan-gökcekoca-442802250/' className='hover:text-teal-500 transition-colors duration-500'><AiFillLinkedin /></a>
+          <a href='https://github.com/oguzhangkckc' className='hover:text-teal-500 transition-colors duration-500'><AiFillGithub /></a>
+          <a href='mailto:ogokcekoca@gmail.com' className='hover:text-teal-500 transition-colors duration-500'><AiFillMail /></a>
+          <a href='https://www.instagram.com/oguzhangokcekoca/' className='hover:text-teal-500 transition-colors duration-500'><AiFillInstagram /></a>
         </div>
       </nav>
     </section>
