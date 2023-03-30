1e1c8485b811c54e3e4d235d3fa2a4cdca75b5bf
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-03-30 20:18:04 +0300

    ekleme

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/your-account/image/upload/',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
