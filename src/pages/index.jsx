1d1fa91733677dc8bd7c89cbfc90c5d3d184a738
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-03-30 22:16:40 +0300

    ekleme

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,7 +15,7 @@ export default function Home() {
         <title>Oğuzhan Gökcekoca</title>
         <meta name="description" content="Welcome to my website!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/fav.ico" />
       </Head>
       <main className='bg-white dark:bg-slate-800'>
         <section className=' min-h-screen'>
