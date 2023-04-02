cb82349816f1a5217ea9d5efcb46b4026114287f
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-04-02 12:02:49 +0300

    eklemeler

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,71 +1,72 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useState } from 'react'
-import { AiFillLinkedin, AiFillGithub, AiFillInstagram, AiFillMail } from 'react-icons/ai'
-import Image from 'next/image'
-import { Link } from 'react-scroll'
-import ogi from "../../images/ogi.png"
+import React, { useState, useEffect } from 'react';
+import { AiFillLinkedin, AiFillGithub, AiFillInstagram, AiFillMail } from 'react-icons/ai';
+import Image from 'next/image';
+import { Link } from 'react-scroll';
+import ogi from '../../images/ogi.png';
 
 export default function NavBar() {
-  const [isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const handleScroll = () => {
     if (window.scrollY > 0) {
-      setIsScrolled(true)
+      setIsScrolled(true);
     } else {
-      setIsScrolled(false)
+      setIsScrolled(false);
     }
-  }
+  };
 
-  React.useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <section
-      className={`w-screen fixed top-0 z-50 ${isScrolled ? "bg-white shadow-lg transition duration-300" : ""
-        }`}
+    <nav
+      className={`fixed top-0 w-full z-50 p-3 flex items-center justify-between ${
+        isScrolled ? 'bg-white shadow-lg transition duration-300' : ''
+      }`}
     >
-      <nav className='items-row w-screen p-3 flex'>
-        <div className='flex gap-4 pl-4 flex-none items-end'>
-          <Image src={ogi} width={35} height={35} className='rounded-full' />
-          <Link to='me' spy={true} smooth={true} duration={500} className='text-2xl font-extrabold dark:text-slate-300 hover:text-teal-500 transition-colors duration-300'>Oğuzhan Gökcekoca</Link>
-        </div>
-        <div className='flex gap-4 flex-grow justify-end'>
-          <ul className='flex gap-28 pr-24 mt-2 font-extrabold dark:text-slate-300'>
-            <li className={`text-lg`}>
-              <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-teal-500 transition-colors duration-300'>
-                About
-              </Link>
-            </li>
-            <li className={`text-lg`}>
-              <Link to='more' spy={true} smooth={true} duration={500} className='hover:text-teal-500 transition-colors duration-300'>
-                More
-              </Link>
-            </li>
-            <li className={`text-lg`}>
-              <Link to='contact' spy={true} smooth={true} duration={500} className='hover:text-teal-500 transition-colors duration-300'>
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className='text-3xl flex gap-8 mt-2 flex-none dark:text-slate-400 '>
-          <a href='https://www.linkedin.com/in/oğuzhan-gökcekoca-442802250/' className='hover:text-teal-500 transition-colors duration-300'>
-            <AiFillLinkedin />
-          </a>
-          <a href='https://github.com/oguzhangkckc' className='hover:text-teal-500 transition-colors duration-300'>
-            <AiFillGithub />
-          </a>
-          <a href='mailto:ogokcekoca@gmail.com' className='hover:text-teal-500 transition-colors duration-300'>
-            <AiFillMail />
-          </a>
-          <a href='https://www.instagram.com/oguzhangokcekoca/' className='hover:text-teal-500 transition-colors duration-300'>
-            <AiFillInstagram />
-          </a>
-        </div>
-      </nav>
-    </section>
-  )
+      <div className="flex items-center gap-4 pl-4">
+        <Image src={ogi} width={35} height={35} className="rounded-full" />
+        <Link to="me" spy={true} smooth={true} duration={500} className="text-2xl font-extrabold dark:text-slate-300 hover:text-teal-500 transition-colors duration-300">
+          Oğuzhan Gökcekoca
+        </Link>
+      </div>
+
+      <ul className="flex items-center gap-8 font-extrabold dark:text-slate-300">
+        <li>
+          <Link to="about" spy={true} smooth={true} duration={500} className="text-lg hover:text-teal-500 transition-colors duration-300">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="more" spy={true} smooth={true} duration={500} className="text-lg hover:text-teal-500 transition-colors duration-300">
+            More
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" spy={true} smooth={true} duration={500} className="text-lg hover:text-teal-500 transition-colors duration-300">
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      <div className="flex items-center gap-8 text-3xl dark:text-slate-400">
+        <a href="https://www.linkedin.com/in/oğuzhan-gökcekoca-442802250/" className="hover:text-teal-500 transition-colors duration-300">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://github.com/oguzhangkckc" className="hover:text-teal-500 transition-colors duration-300">
+          <AiFillGithub />
+        </a>
+        <a href="mailto:ogokcekoca@gmail.com" className="hover:text-teal-500 transition-colors duration-300">
+          <AiFillMail />
+        </a>
+        <a href="https://www.instagram.com/oguzhangokcekoca/" className="hover:text-teal-500 transition-colors duration-300">
+          <AiFillInstagram />
+        </a>
+      </div>
+    </nav>
+  );
 }
--- src/components/sections/header/header.jsx
+++ src/components/sections/header/header.jsx
@@ -1,18 +1,47 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
 
-export default function Header () {
+export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <section id="me">
-      <div className="h-screen">
-        <div className="pt-36 rounded-full text-center flex flex-col items-center">
-          <h2 className="text-4xl py-2 pt-12 text-white"><span className='text-teal-500'>Hello</span>, I'm</h2>
-          <h1 className="text-7xl py-2 text-white font-medium">
+    <section id='me' className={`${isScrolled ? 'bg-white text-teal-500 shadow-lg transition duration-300' : ''}`}>
+      <div className='h-screen'>
+        <div className='pt-36 rounded-full text-center flex flex-col items-center'>
+          <h2 className={`text-4xl py-2 text-white ${isScrolled ? 'text-teal-500' : ''}`}>
+            <span className='text-teal-500'>Hello</span>, I'm
+          </h2>
+          <h1 className={`text-7xl py-2 font-medium text-white ${isScrolled ? 'text-teal-500' : ''}`}>
             Oğuzhan Gökcekoca
           </h1>
-          <h3 className="text-2xl py-2 pt-10 text-white">
-            I am a <span className='text-teal-500'>Software Engineering</span> student<span className="text-teal-500 font-extrabold">.</span>
+          <h3 className={`text-2xl py-2 text-white ${isScrolled ? 'text-teal-500' : ''}`}>
+            I am a{' '}
+            <span className='text-teal-500'>Software Engineering</span> student
+            <span className='text-teal-500 font-extrabold'>.</span>
           </h3>
+          <Link
+            to='more'
+            spy={true}
+            smooth={true}
+            duration={500}
+            className={`bg-teal-500 text-white text-lg font-medium rounded-full px-8 py-3 mt-10 hover:bg-white hover:text-teal-500 transition duration-300 ${isScrolled ? 'border border-teal-500' : ''}`}>
+            Learn More
+          </Link>
         </div>
       </div>
     </section>
