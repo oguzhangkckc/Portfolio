4c0b2655c6ab9f86b25ce756ae848ac15b53f16a
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-04-02 18:33:33 +0300

    ekleme

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
     <>
       <Head>
         <title>Oğuzhan Gökcekoca</title>
-        <link rel="png" href="%PUBLIC_URL%/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <main className='bg-slate-800'>
         <section className=' min-h-screen'>
