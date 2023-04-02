9dfa2d8b9f9232560649ef4336d4c21ed0d753aa
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-04-02 22:54:45 +0300

    ekleme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="shortcut icon" href="/icon/favicon.png" />
+      <link rel="icon" href="/icon/favicon.png" />
       </Head>
       <body>
         <Main />
