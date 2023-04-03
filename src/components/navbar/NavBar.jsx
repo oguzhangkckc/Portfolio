053b74377b344ddec7de3304dc8a7f6a928ca7c7
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-04-03 14:33:22 +0300

    ekleme

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -41,8 +41,8 @@ export default function NavBar() {
     <nav className={`fixed top-0 w-full z-50 p-3 flex items-center justify-between ${isScrolled ? 'bg-white shadow-lg transition duration-300' : ''}`}>
       <div className="flex items-center gap-4 pl-4">
         <Image src={ogi} width={iconSize * 3 / 2} height={iconSize * 3 / 2} className="rounded-full" />
-        <Link to="me" spy={true} smooth={true} duration={500} className={`text-sm sm:text-xs md:text-xl font-extrabold dark:text-slate-300 hover:text-teal-500 transition-colors duration-300 ${isScrolled ? 'truncate' : ''}`}>
-          Oğuzhan Gökcekoca
+        <Link to="me" spy={true} smooth={true} duration={500} className={`text-sm sm:text-xs md:text-xl font-extrabold dark:text-slate-300 hover:text-teal-500 transition-colors duration-300 ${isScrolled ? 'truncate' : ''} whitespace-normal`}>
+          Oğuzhan <br /> Gökcekoca
         </Link>
       </div>
       <div className="flex flex-wrap md:flex-nowrap items-center gap-8 font-extrabold dark:text-slate-300 text-sm md:text-xl list-none">
@@ -62,19 +62,19 @@ export default function NavBar() {
           </Link>
         </li>
       </div>
-      <div className="flex flex-col items-center gap-2 md:flex-row md:gap-8">    
-          <a href="https://www.linkedin.com/in/oğuzhan-gökcekoca-442802250/" className="dark:text-slate-300 hover:text-teal-500 transition-colors duration-300">
-            <AiFillLinkedin size={iconSize} />
-          </a>
-          <a href="https://github.com/oguzhangkckc" className="dark:text-slate-300 hover:text-teal-500 transition-colors duration-300">
-            <AiFillGithub size={iconSize} />
-          </a>
-          <a href="mailto:ogokcekoca@gmail.com" className="dark:text-slate-300 hover:text-teal-500 transition-colors duration-300">
-            <AiFillMail size={iconSize} />
-          </a>
-          <a href="https://www.instagram.com/oguzhangokcekoca/" className="dark:text-slate-300 hover:text-teal-500 transition-colors duration-300">
-            <AiFillInstagram size={iconSize} />
-          </a>   
+      <div className="flex flex-col items-center gap-2 md:flex-row md:gap-8">
+        <a href="https://www.linkedin.com/in/oğuzhan-gökcekoca-442802250/" className="dark:text-slate-300 hover:text-teal-500 transition-colors duration-300">
+          <AiFillLinkedin size={iconSize} />
+        </a>
+        <a href="https://github.com/oguzhangkckc" className="dark:text-slate-300 hover:text-teal-500 transition-colors duration-300">
+          <AiFillGithub size={iconSize} />
+        </a>
+        <a href="mailto:ogokcekoca@gmail.com" className="dark:text-slate-300 hover:text-teal-500 transition-colors duration-300">
+          <AiFillMail size={iconSize} />
+        </a>
+        <a href="https://www.instagram.com/oguzhangokcekoca/" className="dark:text-slate-300 hover:text-teal-500 transition-colors duration-300">
+          <AiFillInstagram size={iconSize} />
+        </a>
       </div>
     </nav>
   );
